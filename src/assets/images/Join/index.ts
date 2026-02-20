832462575da97f6fa3c0ae8d6f5d5597a5a6e33f
Ricky Liang <liangricky7@gmail.com>
2026-02-20 09:15:51 -0600

    feat: add more images to scroll collage

--- src/assets/images/Join/index.ts
+++ src/assets/images/Join/index.ts
@@ -1,18 +1,21 @@
 import AllOurNames from "./CollageLoop/allournames.jpg"
 import CompCamp from "./CollageLoop/compcamp.jpg"
-import FunnyComp from "./CollageLoop/funnycomp.jpg"
 import Kart from "./CollageLoop/kart.jpg"
 import Sammy from "./CollageLoop/sammy.jpg"
 import Spread from "./CollageLoop/spread.jpg"
-import BuildCar from "./CollageLoop/tristan.jpg"
+import Yianni from "./CollageLoop/yianni.jpg"
+import Urwah from "./CollageLoop/urwahpresents.jpg"
+import wtf from "./CollageLoop/wtf.jpg"
+import AJURWAH from "./CollageLoop/AJURWAH.jpg"
 
 export const CollageLoop = {
     Collage1 : AllOurNames,
     Collage2 : CompCamp,
-    Collage3 : FunnyComp,
     Collage4 : Kart,
     Collage5 : Sammy,
     Collage6 : Spread,
-    Collage7 : BuildCar,
-    
+    Collage8 : Yianni,
+    Collage9 : Urwah,
+    Collage10 : wtf,
+    Collage11 : AJURWAH
 }
